d1463c2decbad5028e17cf0edcf75c9a6dd6b9c9
James Clyde Baritua <53964526+jcbaritua@users.noreply.github.com>
2022-06-04 18:50:44 +0800

    update dependencies

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import cors from "cors";
-import { userRoute } from "./prisma/userroutes";
-import { petRoute } from "./prisma/petroutes";
+import { userRoute } from "./routes/userroutes.js";
+import { petRoute } from "./routes/petroutes.js";
 
 const app = express();
 
-app.use(express.json())
-app.use("api/users", userRoute)
-app.use("api/pets", petRoute)
-app.use(cors({
-    origin: "localhost"
-}))
+app.use(express.json());
+app.use("api/users", userRoute);
+app.use("api/pets", petRoute);
+app.use(
+  cors({
+    origin: "localhost",
+  })
+);
 
-app.listen(8000, ()=>{
-    console.log("Listening on port 8000")
-}) 
+app.listen(8000, () => {
+  console.log("Listening on port 8000");
+});
