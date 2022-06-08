b3c72062de12169c395febc6907cc2e4ffc83458
jcbaritua <53964526+potatofries-xyz@users.noreply.github.com>
2022-06-09 07:25:52 +0800

    deleted files

--- server/prisma/petroutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
-
-const petRoute = Router()
-const { pet } = new PrismaClient()
-
-export { petRoute }
--- server/prisma/userroutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
-import jsonwebtoken from "jsonwebtoken";
-
-const userRoute = Router()
-const jwt = jsonwebtoken
-const { user } = new PrismaClient()
-
-export { userRoute }
