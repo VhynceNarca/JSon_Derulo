493b2cc57fe0d71e7af1449767095f4dcb5237d9
Aimilli Canoy <ayaserizawaendo@gmail.com>
2022-06-11 01:20:15 +0800

    fix navbar route

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import Dogs from "./pages/Dogs";
 import Cats from "./pages/Cats";
 import Home from "./pages/Index";
+import WithNav from "./components/WithNav";
+import WithoutNav from "./components/WithoutNav";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LogIn />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/dogs" element={<Dogs />} />
-        <Route path="/cats" element={<Cats />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route element={<WithoutNav />}>
+          <Route path="/" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+        <Route element={<WithNav />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/dogs" element={<Dogs />} />
+          <Route path="/cats" element={<Cats />} />
+        </Route>
       </Routes>
     </Router>
   );
