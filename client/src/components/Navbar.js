c37f5c3cf8a5639ad420cad7292c71f0e7795899
chezol <citchezol@gmail.com>
2022-06-12 23:50:10 +0800

    click logout to clear local storage

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,30 +1,30 @@
-import "./navbar.css"
-import { Link } from "react-router-dom"
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <nav className='navbar'>
+      <nav className="navbar">
         <div className="logodiv">
-          <img src="/logo.png" className="imglogo" alt="logo"/>
-          <h3 className='logo'>Paw Society</h3>
+          <img src="/logo.png" className="imglogo" alt="logo" />
+          <h3 className="logo">Paw Society</h3>
         </div>
         <ul className="nav-links">
-          <Link to='/home'>
+          <Link to="/home">
             <li>Home</li>
           </Link>
-          <Link to='/dogs'>
+          <Link to="/dogs">
             <li>Dogs</li>
           </Link>
-          <Link to='/cats'>
+          <Link to="/cats">
             <li>Cats</li>
           </Link>
-          <Link to='/'>
+          <Link to="/" onClick={() => localStorage.clear()}>
             <li>Logout</li>
           </Link>
         </ul>
       </nav>
     </>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
